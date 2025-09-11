40d4ef4a9d58c5ca250e2cfbd7132c18396ee54e
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-11 17:33:28 -0300

    Finalizando o projeto

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 import pyautogui
 
 pyautogui.FAILSAFE = False
+SENSIBILIDADE_DO_MODELO = 3
 
 # Lendo a câmera e inicializando a solução
 cam = cv2.VideoCapture(0)
@@ -32,9 +33,26 @@ while True:
 
         # Checar se a boca esta aberta
         distancia_da_boca = iris_and_mouth[-2].y - iris_and_mouth[-1].y
-        print(distancia_da_boca)
+        #print(distancia_da_boca)
 
-        for lm in landmarks:
+        if distancia_da_boca > 0.042:
+            pass
+        else:
+            iris_principal = iris_and_mouth[0]
+            x = int(iris_principal.x * frame_w) * SENSIBILIDADE_DO_MODELO
+            y = int(iris_principal.y * frame_h) * SENSIBILIDADE_DO_MODELO
+
+            pyautogui.moveTo(x, y)
+
+            # Logica de clique
+            distancia_da_iris = iris_and_mouth[0].y - iris_and_mouth[1].y
+
+            if distancia_da_iris < 0.008:
+                pyautogui.click()
+                pyautogui.sleep(1)
+
+
+        for lm in iris_and_mouth:
             x = int(lm.x * frame_w)
             y = int(lm.y * frame_h)
             cv2.circle(img, (x, y), 2, (255, 255, 0))
